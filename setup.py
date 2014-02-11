df4b46ff882dd332904d19e07544822f168fb514
Wyatt Lee Baldwin <wyatt.lee.baldwin@gmail.com>
2014-02-10 19:02:40 -0800

    Fix download_url in setup()
    Should point to /tags

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(
     description='Tangled Mako integration',
     long_description=open('README.rst').read(),
     url='http://tangledframework.org/',
-    download_url='https://github.com/TangledWeb/tangled.mako',
+    download_url='https://github.com/TangledWeb/tangled.mako/tags',
     author='Wyatt Baldwin',
     author_email='self@wyattbaldwin.com',
     include_package_data=True,
