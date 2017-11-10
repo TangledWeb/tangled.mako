40c8b74d5ce7dd3b76fe878eb769b95cea3f40c0
Wyatt Baldwin <self@wyattbaldwin.com>
2017-11-09 18:56:29 -0800

    Upgrade tangled.web 0.1a5 => 0.1a10

--- setup.py
+++ setup.py
@@ -17,12 +17,12 @@ setup(
         'tangled.mako.tests',
     ],
     install_requires=[
-        'tangled.web>=0.1a5',
+        'tangled.web>=0.1a10',
         'Mako>=1.0',
     ],
     extras_require={
         'dev': [
-            'tangled.web[dev]>=0.1a5',
+            'tangled.web[dev]>=0.1a10',
         ],
     },
     classifiers=[
