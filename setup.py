82734e5db338ad5636c2ca565532ba21f1e5ab25
Wyatt Baldwin <self@wyattbaldwin.com>
2017-12-10 17:48:25 -0800

    Back to development: 1.0a6.dev0

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name='tangled.mako',
-    version='1.0a5',
+    version='1.0a6.dev0',
     description='Tangled Mako integration',
     long_description=open('README.rst').read(),
     url='http://tangledframework.org/',
